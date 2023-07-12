463d5d82918474a04b0aacbc79cab3d3dc5f0d2e
Pablo Hebenstreit Gotelli <pablo2e@yahoo.com>
2023-07-12 19:59:47 +0200

    seccion2 clase6-7

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = 'Svelte';
+  let src="http://picsum.com/200/200"
+  let string = 'Some <strong>string</strong> with html.'
+</script>
+
+<img {src} alt="Some text {name}"/>
+<h1>Hello {name.toUpperCase()}!</h1>
+<p>{@html string}</p><!-- @html permite que se lea la etiqueta html del string -->
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
