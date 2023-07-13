eb1988720fa4a6bc8df75a839bf9f9c7e8e157e9
Pablo Hebenstreit Gotelli <pablo2e@yahoo.com>
2023-07-13 19:27:51 +0200

    seccion2 clase8-9

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,9 @@
 <script>
-  let name = 'Svelte';
-  let src="http://picsum.com/200/200"
-  let string = 'Some <strong>string</strong> with html.'
+  import Counter from './lib/Counter.svelte'
 </script>
 
-<img {src} alt="Some text {name}"/>
-<h1>Hello {name.toUpperCase()}!</h1>
-<p>{@html string}</p><!-- @html permite que se lea la etiqueta html del string -->
+<Counter />
 
 <style>
-  h1 {
-    color: red;
-  }
+
 </style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,28 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
+  let count = 0;
+  let string = `You have clicked ${count} times`;
+
+  /* Reactive Statement */
+  $: stringReactivo = `You have clicked ${count} times`;
+
+  $: if(count > 5) {
+    console.log( `the count is ${count}`);
+  }
+
+  function increment () {
+    count ++
   }
 </script>
 
 <button on:click={increment}>
   count is {count}
 </button>
+<h3>{string}</h3><!-- es estatico y no se actualiza, no es reactivo -->
+<h3>You have clicked {count} times</h3><!-- se actualiza, es reactivo  -->
+<h3>{stringReactivo}</h3><!-- se actualiza, es reactivo  -->
+
+<style>
+  button {
+    padding: 15px 20px;
+  }
+</style>
