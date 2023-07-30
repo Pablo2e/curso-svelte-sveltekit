2d5113ccdb557a50fa8ab7e09ad28e4897a60ce5
Pablo Hebenstreit Gotelli <pablo2e@yahoo.com>
2023-07-30 17:01:27 +0200

    seccion4 21 binding input value to a variable

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -1,12 +1,25 @@
 <script>
   export let todos = [];
+  import Button from "./Button.svelte";
+  let inputText = '';
+
+  function handleAddTodo(){
+    console.log(inputText);
+  }
 </script>
 
-<ul>
-  <!--{#each todos as todo, index (todo.id)}  (todo.id) para identificar el loop -->
-  {#each todos as {title, id}, index (id)} <!-- se puede desestructurar el todo -->
-    {@const number = index + 1} <!-- 2 formas de pintar el index -->
-    <li>{number} - {title}</li>
-    <!-- <li>{index + 1} - {todo.title}</li> -->
-  {/each}
-</ul>
+<div class="todo-list-wrapper">
+  <ul>
+    {#each todos as {title, id}, index (id)}
+      {@const number = index + 1} 
+      <li>{number} - {title}</li>
+    {/each}
+  </ul>
+</div>
+
+{inputText}
+
+<form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
+  <input bind:value={inputText}/>
+  <Button type="submit">Add</Button>
+</form>
