beb27eed66d0d7590c163ceb8cb584465b356926
Pablo Hebenstreit Gotelli <pablo2e@yahoo.com>
2023-07-30 13:46:41 +0200

    seccion4 20 each loops

---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let todos = [];
+</script>
+
+<ul>
+  <!--{#each todos as todo, index (todo.id)}  (todo.id) para identificar el loop -->
+  {#each todos as {title, id}, index (id)} <!-- se puede desestructurar el todo -->
+    {@const number = index + 1} <!-- 2 formas de pintar el index -->
+    <li>{number} - {title}</li>
+    <!-- <li>{index + 1} - {todo.title}</li> -->
+  {/each}
+</ul>
