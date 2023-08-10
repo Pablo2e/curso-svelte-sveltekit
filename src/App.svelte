3fc8d10f669f1306065684454d6b7de2f1f1a78e
Pablo Hebenstreit Gotelli <pablo2e@yahoo.com>
2023-08-10 20:44:50 +0200

    seccion4 41 making the component more customizable with slots

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import TodoList from './lib/TodoList.svelte';
   import { v4 as uuid } from 'uuid';
   import { tick, onMount } from 'svelte';
+  import { identity } from 'svelte/internal';
 
   let todoList;
   let showList = true;
@@ -117,7 +118,26 @@
       on:addtodo={handleAddTodo}
       on:removetodo={handleRemoveTodo}
       on:toggletodo={handleToggleTodo}
-    />
+      let:todo
+      let:handleToggleTodo
+      let:index
+    >
+      <svelte:fragment slot="title">{index + 1}- {todo.title}</svelte:fragment>
+      <!-- {@const { id, completed, title } = todo} -->
+      <!-- <Todo {todo} on:remove on:toggle /> -->
+      <!-- <div>
+        <input
+          disabled={disabledItems.includes(id)}
+          on:input={(event) => {
+            event.currentTarget.checked = completed;
+            handleToggleTodo(id, !completed);
+          }}
+          type="checkbox"
+          checked={completed}
+        />
+        {title}
+      </div> -->
+    </TodoList>
   </div>
 {/if}
 
