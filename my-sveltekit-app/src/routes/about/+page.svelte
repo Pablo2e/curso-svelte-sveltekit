0d26034221a6b616dce6702892269b025cf31921
Pablo Hebenstreit Gotelli <pablo2e@yahoo.com>
2023-10-15 11:15:56 +0200

    seccion 10 89 navigation functions: goto(), beforeNavigate() & afterNavigate()

--- my-sveltekit-app/src/routes/about/+page.svelte
+++ my-sveltekit-app/src/routes/about/+page.svelte
@@ -1,1 +1,15 @@
-<h1>About</h1>
+<script>
+	import { goto } from '$app/navigation';
+
+	let name = 'John';
+</script>
+
+<h1>About {name}</h1>
+
+<button
+	on:click={() => {
+		goto('/');
+	}}>Go to Home</button
+>
+
+<style></style>
